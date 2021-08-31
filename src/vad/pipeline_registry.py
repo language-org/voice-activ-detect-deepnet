9042660858b9c397ecdc750da9782a8c45601beb
steeve <steeve@stanford.edu>
2021-08-31 13:54:49 +0200

    Call pipes from a central registry

--- src/vad/pipeline_registry.py
+++ src/vad/pipeline_registry.py
@@ -30,6 +30,10 @@
 from typing import Dict
 
 from kedro.pipeline import Pipeline
+from vad.pipelines.data_eng import pipeline as data_eng
+from vad.pipelines.train import pipeline as train
+from vad.pipelines.inference import pipeline as inference
+from vad.pipelines.evaluate import pipeline as evaluate
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -38,4 +42,11 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
-    return {"__default__": Pipeline([])}
+    return {
+        "data_eng": Pipeline([data_eng.run_for_train()]),
+        "train": Pipeline([data_eng.run_for_train(), train.run()]),
+        "predict": Pipeline([data_eng.run_for_inference(), inference.run()]),
+        "predict_and_eval": Pipeline(
+            [data_eng.run_for_inference(), inference.run(), evaluate.run()]
+        ),
+    }
